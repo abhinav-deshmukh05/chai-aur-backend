ca43d93ebb490b78f332090abfacc029d3440aa0
AbhinavDeshmukh123 <abhinavdeshmukhara@gmail.com>
2024-09-04 23:41:30 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import dotenv from "dotenv";
-
+dotenv.config();
 
 import express from "express";
 import connectDB from "./db/index.js";
 
 const app = express();
 
-// Connect to MongoDB
-connectDB();
+
+// Event listener for uncaught exceptions
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err.message);
+    console.error(err.stack);
+    // Optionally, exit the process or take other actions
+    process.exit(1); // This is usually done to stop the server due to an unrecoverable error
+});
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection error: ", err);
+    process.exit(1);
+});
 
 // Middleware to parse JSON
 app.use(express.json());
